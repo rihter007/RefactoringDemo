bd99119f5ddcc51808808b43005ca7d5d3f020ca
rihter007 <rihter007@inbox.ru>
2015-11-20 11:32:45 +0300

    Minor cs changes

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,13 +1,15 @@
 /**
  * Created by Rihter on 19.11.2015.
+ * Unit tests for Customer class
  */
+import org.junit.Assert;
+import org.junit.Test;
+
 import com.scrumtrek.simplestore.Customer;
 import com.scrumtrek.simplestore.Movie;
 import com.scrumtrek.simplestore.PriceCodes;
 import com.scrumtrek.simplestore.Rental;
 import com.sun.istack.internal.NotNull;
-import org.junit.Assert;
-import org.junit.Test;
 
 public class CustomerTest
 {
